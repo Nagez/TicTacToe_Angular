de48ba8753751e28c8af6c061df6359ba4a7acb2
Nagez <81879748+Nagez@users.noreply.github.com>
2023-11-23 20:28:17 +0200

    initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { SquareComponent } from "./square/square.component";
+import { BoardComponent } from "./board/board.component";
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    imports: [CommonModule, RouterOutlet, SquareComponent, BoardComponent]
+})
+export class AppComponent {
+  title = 'myapp';
+}
